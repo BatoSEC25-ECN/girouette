d89a7238964330aefae89e6a71ca93b6cd0c29ad
Guillaume de Maricourt <51048232+Widelx@users.noreply.github.com>
2025-06-25 22:01:27 +0200

    Measurement tools and minors patches (#6)
    * doc: add fusion360 download page
    
    * chore: update gitignore and required tools
    
    * doc: add keysight developper datasheet
    
    * doc: improve setup documentation
    
    * chore: add python libraries requirements
    
    * feat!: create MCU pwm burst generation
    
    * feat!: add keysight custom library and required dataclasses
    
    * feat: add a logger for debug purpose
    
    * feat: create a plotter to compare two data curve
    
    * feat!: create main runner script
    
    * fix: remove uncrustify config (duplicated, wrong folder)
    
    * refactor: remove unused dependencies
    
    * feat!: add possibility of running multiple tests
    
    Add multiple features to run "infinite" amount of tests:
     - plotter handles an undefined amount of .csv files from a list (restr
       icted to 5 colors though, curves color could overlap)
     - keysight file formatting is standardized (lower case, ensure .csv at
       the end, replace spaces with underscore)
     - main is updated to run an undefined amount of measurement, prompting
       the user to run the next test
    
    * refactor(dataclasses): improve readability and split some functions logic
    
    * refactor(keysight): improve readability and split some functions logic
    
    * refactor(plotter): improve readability and split some functions logic
    
    * refactor: update main to match previous changes
    
    * fix:clean probe ratio
    
    D:\Documents\41_Ecole\4_Centrale_Nantes\A3\_Projet_bateau\girouette\code\measure_tool\main.py
    
    * refactor(dataclasses): refactor code
    
    D:\Documents\41_Ecole\4_Centrale_Nantes\A3\_Projet_bateau\girouette\code\measure_tool\dataclass.py
    
    * feat: reimplement probe ratio
    
    D:\Documents\41_Ecole\4_Centrale_Nantes\A3\_Projet_bateau\girouette\code\measure_tool\dataclass.pyD:\Documents\41_Ecole\4_Centrale_Nantes\A3\_Projet_bateau\girouette\code\measure_tool\keysight.py
    
    * refactor(keysight): refactor code
    
    * fix: fix plotter issues
    
    D:\Documents\41_Ecole\4_Centrale_Nantes\A3\_Projet_bateau\girouette\code\measure_tool\plotter.py
    
    * fix: patch frequency clamping
    
    * feat: add horizontal markers
    
    * close project

---
+++ code/measure_tool/main.py
@@ -0,0 +1,98 @@
+# pylint: disable = missing-module-docstring, missing-function-docstring
+
+import logging
+import time
+from logger import CustomFormatter
+
+import keysight as ks
+from plotter import plot_collected_data
+
+from dataclass import (
+    KeysightConfig,
+    Channel,
+    TimeUnit,
+    Trigger,
+    TriggerSource,
+    SlopeType,
+    VoltageUnit,
+    FrequencyUnit,
+)
+
+# Create the logger
+# call:
+# "logger = logging.getLogger(__name__)"
+# at the top of your dependency to use the same shared logger instance (shared format)
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setFormatter(CustomFormatter())
+logging.basicConfig(level=logging.INFO, handlers={handler})
+
+channel1 = Channel(
+    number=1,
+    name="EMITTER",
+    vertical_scale=2,
+    vertical_unit=VoltageUnit.V,
+)
+
+channel2 = Channel(
+    number=2,
+    name="RCVR_L",
+    vertical_scale=12.5,
+    vertical_unit=VoltageUnit.mV,
+)
+
+channel3 = Channel(
+    number=3,
+    name="RCVR_R",
+    vertical_scale=50,
+    vertical_unit=VoltageUnit.mV,
+)
+
+# Create trigger
+trigger = Trigger(
+    source=TriggerSource.CHANNEL_1,
+    slope=SlopeType.POSITIVE,
+    threshold=2,
+    threshold_unit=VoltageUnit.V,
+)
+
+# Create Keysight configuration
+keysight_config = KeysightConfig(
+    # channels=[channel2, channel3, channel4],
+    channels=[channel1, channel2, channel3],
+    trigger=trigger,
+    frequency=1,
+    frequency_unit=FrequencyUnit.MHz,
+    horizontal_range=2,
+    horizontal_unit=TimeUnit.MS,
+)
+
+MEASURES_NAME = [
+    "Test without wind",
+    "Test with wind",
+]
+OUTPUT_DIR = "measurements"
+
+
+def main():
+    output_files = ["test_without_wind.csv", "test_with_wind.csv"]
+
+    device = ks.KeysightDevice(keysight_config)
+    device.connect()
+    device.setup()
+
+    for name in MEASURES_NAME:
+        time.sleep(4)  # Wait for the device to be ready
+        # input(f'Press enter to run next test, named: \n "{name}".')
+
+        device.collect()
+        f = device.save_measures(OUTPUT_DIR, name)
+        output_files.append(f)
+
+    device.release()
+
+    plot_collected_data(OUTPUT_DIR, output_files, "measurements.html")
+
+
+if __name__ == "__main__":
+    main()
